08a7156986b0338bfb672f538316c75fd6b42a07
Sofia <asofimp@gmail.com>
2023-10-27 12:26:26 -0600

    Administrador - Crecion de frontend

--- Frontend/Administrador.Designer.cs
+++ Frontend/Administrador.Designer.cs
@@ -32,13 +32,10 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnReporteAdmi = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnRegistroCurso = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.btnRegistroCarrera = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,7 +45,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("MingLiU-ExtB", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(291, 160);
+            this.label2.Location = new System.Drawing.Point(353, 147);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(163, 29);
@@ -61,7 +58,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(252, 104);
+            this.label1.Location = new System.Drawing.Point(311, 91);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(246, 40);
@@ -71,67 +68,32 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(624, 44);
+            this.pictureBox1.Location = new System.Drawing.Point(737, 16);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(244, 248);
+            this.pictureBox1.Size = new System.Drawing.Size(131, 115);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 40;
             this.pictureBox1.TabStop = false;
             // 
-            // btnReporteAdmi
+            // btnRegistroCurso
             // 
-            this.btnReporteAdmi.BackColor = System.Drawing.Color.SlateGray;
-            this.btnReporteAdmi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnReporteAdmi.Cursor = System.Windows.Forms.Cursors.Default;
-            this.btnReporteAdmi.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
-            this.btnReporteAdmi.FlatAppearance.BorderSize = 0;
-            this.btnReporteAdmi.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateGray;
-            this.btnReporteAdmi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnReporteAdmi.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReporteAdmi.ForeColor = System.Drawing.Color.White;
-            this.btnReporteAdmi.Location = new System.Drawing.Point(139, 256);
-            this.btnReporteAdmi.Name = "btnReporteAdmi";
-            this.btnReporteAdmi.Size = new System.Drawing.Size(137, 55);
-            this.btnReporteAdmi.TabIndex = 44;
-            this.btnReporteAdmi.Text = "Carrera";
-            this.btnReporteAdmi.UseVisualStyleBackColor = false;
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.SlateGray;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateGray;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(139, 340);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 55);
-            this.button1.TabIndex = 45;
-            this.button1.Text = "Ciclo";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.SlateGray;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateGray;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(349, 256);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(137, 55);
-            this.button2.TabIndex = 46;
-            this.button2.Text = "Curso";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnRegistroCurso.BackColor = System.Drawing.Color.SlateGray;
+            this.btnRegistroCurso.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnRegistroCurso.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnRegistroCurso.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnRegistroCurso.FlatAppearance.BorderSize = 0;
+            this.btnRegistroCurso.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateGray;
+            this.btnRegistroCurso.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegistroCurso.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistroCurso.ForeColor = System.Drawing.Color.White;
+            this.btnRegistroCurso.Location = new System.Drawing.Point(370, 245);
+            this.btnRegistroCurso.Name = "btnRegistroCurso";
+            this.btnRegistroCurso.Size = new System.Drawing.Size(137, 55);
+            this.btnRegistroCurso.TabIndex = 45;
+            this.btnRegistroCurso.Text = "Registro \r\nCurso";
+            this.btnRegistroCurso.UseVisualStyleBackColor = false;
+            this.btnRegistroCurso.Click += new System.EventHandler(this.btnRegistroCurso_Click);
             // 
             // button3
             // 
@@ -144,48 +106,13 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(349, 340);
+            this.button3.Location = new System.Drawing.Point(546, 245);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(137, 55);
             this.button3.TabIndex = 47;
-            this.button3.Text = "Periodo";
+            this.button3.Text = "Reportes";
             this.button3.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.SlateGray;
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button4.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
-            this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateGray;
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button4.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(537, 256);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(137, 55);
-            this.button4.TabIndex = 48;
-            this.button4.Text = "Sección";
-            this.button4.UseVisualStyleBackColor = false;
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.SlateGray;
-            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button5.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
-            this.button5.FlatAppearance.BorderSize = 0;
-            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateGray;
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button5.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.ForeColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(537, 340);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(137, 55);
-            this.button5.TabIndex = 49;
-            this.button5.Text = "Extensión";
-            this.button5.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button6
             // 
@@ -198,12 +125,31 @@
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button6.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.ForeColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(731, 340);
+            this.button6.Location = new System.Drawing.Point(782, 395);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(137, 55);
+            this.button6.Size = new System.Drawing.Size(101, 29);
             this.button6.TabIndex = 50;
             this.button6.Text = "Salir";
             this.button6.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistroCarrera
+            // 
+            this.btnRegistroCarrera.BackColor = System.Drawing.Color.SlateGray;
+            this.btnRegistroCarrera.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnRegistroCarrera.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnRegistroCarrera.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnRegistroCarrera.FlatAppearance.BorderSize = 0;
+            this.btnRegistroCarrera.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateGray;
+            this.btnRegistroCarrera.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegistroCarrera.Font = new System.Drawing.Font("Arial Unicode MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistroCarrera.ForeColor = System.Drawing.Color.White;
+            this.btnRegistroCarrera.Location = new System.Drawing.Point(191, 245);
+            this.btnRegistroCarrera.Name = "btnRegistroCarrera";
+            this.btnRegistroCarrera.Size = new System.Drawing.Size(137, 55);
+            this.btnRegistroCarrera.TabIndex = 51;
+            this.btnRegistroCarrera.Text = "Registro Carrera";
+            this.btnRegistroCarrera.UseVisualStyleBackColor = false;
+            this.btnRegistroCarrera.Click += new System.EventHandler(this.btnRegistroCarrera_Click);
             // 
             // Administrador
             // 
@@ -211,13 +157,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.ClientSize = new System.Drawing.Size(895, 436);
+            this.Controls.Add(this.btnRegistroCarrera);
             this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnReporteAdmi);
+            this.Controls.Add(this.btnRegistroCurso);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -235,12 +178,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btnReporteAdmi;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnRegistroCurso;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button btnRegistroCarrera;
     }
 }
